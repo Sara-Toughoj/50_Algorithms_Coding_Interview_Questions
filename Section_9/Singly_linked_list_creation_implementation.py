758f564c8c7cc38b3af2f1a55d0e9f1817eead2f
Sara-Toughoj <ss.toughoj@gmail.com>
2021-12-11 16:09:37 +0200

    SECTION_9 Singly Liked List Insert + delete Implementation

--- Section_9/Singly_linked_list_creation_implementation.py
+++ Section_9/Singly_linked_list_creation_implementation.py
@@ -18,6 +18,58 @@ class LinkedList:
 
         print(linked_list)
 
+    def insertNode(self, value, position):
+        target = Node(value)
+
+        if position == 0:
+            target.next = self.head
+            self.head = target
+            return
+
+        position_counter = 1
+        temp_node = self.head
+
+        while position_counter != position:
+            temp_node = temp_node.next
+            if temp_node is None:
+                print("cant insert out of list's range")
+                return
+            position_counter += 1
+
+        previous_node = self.getPreviousNode(position)
+        target.next = previous_node.next
+        previous_node.next = target
+
+    def getPreviousNode(self, position):
+        position_counter = 1
+        temp_node = self.head
+
+        while position_counter < position:
+            temp_node = temp_node.next
+            position_counter += 1
+
+        return temp_node
+
+    def deleteNode(self, key):
+        temp = self.head
+        if temp is None:
+            print("node not found")
+            return
+
+        if temp.data == key:
+            target_node = self.head
+            self.head = self.head.next
+            target_node = None
+            return
+
+        while temp.next is not None and temp.next.data != key:
+            temp = temp.next
+
+        if temp.next is not None and temp.next.data == key:
+            target_node = temp.next
+            temp.next = target_node.next
+            target_node.next = None
+
 
 linked_list = LinkedList()
 linked_list.head = Node(5)
@@ -30,4 +82,8 @@ linked_list.head.next = second_node
 second_node.next = third_node
 third_node.next = fourth_node
 
+linked_list.printList()
+
+linked_list.insertNode(10, 90)
+linked_list.deleteNode(5)
 linked_list.printList()
